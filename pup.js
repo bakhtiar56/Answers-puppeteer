8a8ad0e336bbbfa86e15b6f58f2b090026b6ffda
Bakhtiar Rasheed <81223935+bakhtiar56@users.noreply.github.com>
2023-08-17 13:58:46 +0500

    Create pup.js

---
+++ pup.js
@@ -0,0 +1,36 @@
+import puppeteer from 'puppeteer';
+
+const authenticate = async () => {
+    // Launch the browser and open a new blank page
+    const browser = await puppeteer.launch({
+        headless: true,
+        args: [
+            "--no-sandbox",
+            "--disable-gpu",
+            '--disable-setuid-sandbox'
+        ]
+    });
+    const page = await browser.newPage();
+    const username = "YOUR_USERNAME_HERE"
+    const password = "YOUR_PASSWORD_HERE"
+
+    // Navigate the page to a URL
+    await page.goto('https://github.com/login');
+    await page.type("#login_field", username)
+    await page.type("#password", password)
+    await page.click("[type=submit]") // Click on submit button
+
+    const ss = await page.screenshot({ path: "screenshot.png" }); // Take screenshot of the page
+
+
+
+
+
+    await page.close();
+
+
+
+    await browser.close();
+};
+
+authenticate()
